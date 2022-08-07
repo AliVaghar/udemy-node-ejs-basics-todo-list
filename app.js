bf1a00d4cbe5c495d71a7dbe4486a352850990ee
Ali Ghasemivaghar <ag3904@columbia.edu>
2022-08-06 21:35:34 -0500

    to do list added

--- app.js
+++ app.js
@@ -3,31 +3,30 @@ const bodyParser = require("body-parser");
 
 app = express();
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 
-function getStringDay(numberDay) {
-  switch(numberDay) {
-    case 1:
-      return 'Monday';
-    case 2:
-      return 'Tuesday';
-    case 3:
-      return 'Wednesday';
-    case 4:
-      return 'Thursday';
-    case 5:
-      return 'Friday';
-    case 6:
-      return 'Saturday';
-    case 7:
-      return 'Sunday';
-  }
-}
+// use all of the contents in public folder
+app.use(express.static("public"));
+
+var items = [];
+
+function addItem(item) {
+  items.push(item);
+};
 
 app.get("/", function(req, res) {
-  let day = new Date();
-  res.render('index', {TODAY: getStringDay(day.getDay())});
+  let options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+  let day = new Date().toLocaleDateString('en-us', options);
+  console.log(items);
+  res.render('index', {TODAY: day, TASKS: items});
 })
+
+app.post("/add", function(req, res) {
+  let newTask = req.body.task;
+  addItem(newTask);
+  res.redirect("/");
+});
 
 app.listen(3000, function(){
 })
